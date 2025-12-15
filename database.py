493ca37df9bc22cee1752b4addb2f54c48392d79
Nazilazare <nazila.zare@gmail.com>
2025-12-15 10:56:00 +0100

    Add posts database functions (create, get all, get by ID)

--- database.py
+++ database.py
@@ -15,3 +15,25 @@ def init_db():
         conn.executescript(f.read())
     conn.commit()
     conn.close()
+
+# Posts functions
+def create_post(title, content):
+    """Create a new blog post."""
+    conn = get_db_connection()
+    conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+    conn.commit()
+    conn.close()
+
+def get_all_posts():
+    """Get all blog posts."""
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts ORDER BY created_at DESC').fetchall()
+    conn.close()
+    return posts
+
+def get_post_by_id(post_id):
+    """Get a single post by ID."""
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+    return post
