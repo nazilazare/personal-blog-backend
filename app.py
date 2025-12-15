da2f93794a72742a3b1a8e6610e18becde6c88b7
Nazilazare <nazila.zare@gmail.com>
2025-12-15 10:07:00 +0100

    Add GET /posts endpoint to retrieve all blog posts

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import database
 
 app = Flask(__name__)
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 def home():
     """Home endpoint."""
     return jsonify({"message": "Personal Blog API"})
+
+@app.route('/posts', methods=['GET'])
+def get_posts():
+    """Get all posts."""
+    posts = database.get_all_posts()
+    return jsonify([dict(post) for post in posts])
 
 if __name__ == '__main__':
     app.run(debug=True)
