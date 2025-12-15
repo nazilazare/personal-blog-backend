42fb61948acd90d07c6a28bb7c47abcc048bb61d
Nazilazare <nazila.zare@gmail.com>
2025-12-15 11:59:00 +0100

    Add database functions for tags and update comment creation

--- database.py
+++ database.py
@@ -39,11 +39,11 @@ def get_post_by_id(post_id):
     return post
 
 # Comments functions
-def create_comment(post_id, author, content):
+def create_comment(post_id, author, title, content):
     """Create a new comment for a post."""
     conn = get_db_connection()
-    conn.execute('INSERT INTO comments (post_id, author, content) VALUES (?, ?, ?)', 
-                 (post_id, author, content))
+    conn.execute('INSERT INTO comments (post_id, author, title, content) VALUES (?, ?, ?, ?)', 
+                 (post_id, author, title, content))
     conn.commit()
     conn.close()
 
@@ -54,3 +54,68 @@ def get_comments_by_post(post_id):
                            (post_id,)).fetchall()
     conn.close()
     return comments
+
+# Tags functions
+def create_tag(name):
+    """Create a new tag."""
+    conn = get_db_connection()
+    try:
+        conn.execute('INSERT INTO tags (name) VALUES (?)', (name,))
+        conn.commit()
+    except:
+        pass  # Tag already exists
+    conn.close()
+
+def get_or_create_tag(name):
+    """Get a tag by name or create it if it doesn't exist."""
+    conn = get_db_connection()
+    tag = conn.execute('SELECT * FROM tags WHERE name = ?', (name,)).fetchone()
+    if tag is None:
+        conn.execute('INSERT INTO tags (name) VALUES (?)', (name,))
+        conn.commit()
+        tag = conn.execute('SELECT * FROM tags WHERE name = ?', (name,)).fetchone()
+    conn.close()
+    return tag
+
+def add_tag_to_post(post_id, tag_name):
+    """Add a tag to a post."""
+    tag = get_or_create_tag(tag_name)
+    conn = get_db_connection()
+    try:
+        conn.execute('INSERT INTO post_tags (post_id, tag_id) VALUES (?, ?)', 
+                     (post_id, tag['id']))
+        conn.commit()
+    except:
+        pass  # Tag already associated with post
+    conn.close()
+
+def get_tags_for_post(post_id):
+    """Get all tags for a specific post."""
+    conn = get_db_connection()
+    tags = conn.execute('''
+        SELECT t.* FROM tags t
+        JOIN post_tags pt ON t.id = pt.tag_id
+        WHERE pt.post_id = ?
+    ''', (post_id,)).fetchall()
+    conn.close()
+    return tags
+
+def get_posts_by_tag(tag_name):
+    """Get all posts for a specific tag."""
+    conn = get_db_connection()
+    posts = conn.execute('''
+        SELECT p.* FROM posts p
+        JOIN post_tags pt ON p.id = pt.post_id
+        JOIN tags t ON pt.tag_id = t.id
+        WHERE t.name = ?
+        ORDER BY p.created_at DESC
+    ''', (tag_name,)).fetchall()
+    conn.close()
+    return posts
+
+def get_all_tags():
+    """Get all tags."""
+    conn = get_db_connection()
+    tags = conn.execute('SELECT * FROM tags ORDER BY name').fetchall()
+    conn.close()
+    return tags
