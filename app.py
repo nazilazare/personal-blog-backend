8f202415502d4239289e168df3b8b4ec3dc81a53
Nazilazare <nazila.zare@gmail.com>
2025-12-15 10:19:00 +0100

    Add GET /posts/<id> endpoint to retrieve single post by ID

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def get_posts():
     posts = database.get_all_posts()
     return jsonify([dict(post) for post in posts])
 
+@app.route('/posts/<int:post_id>', methods=['GET'])
+def get_post(post_id):
+    """Get a single post by ID."""
+    post = database.get_post_by_id(post_id)
+    if post is None:
+        return jsonify({"error": "Post not found"}), 404
+    return jsonify(dict(post))
+
 if __name__ == '__main__':
     app.run(debug=True)
